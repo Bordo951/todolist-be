bc27ee740c285cc9720fcf5e2995dc0ebb091af5
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-06-20 02:56:09 +0300

    feat: add todo database model

---
+++ todolist-be/src/models/todo.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+interface ITodo {
+    message: string,
+    completed: boolean,
+}
+
+interface TodoModelInterface extends mongoose.Model<TodoDoc> {
+    build(attr: ITodo): TodoDoc;
+}
+
+interface TodoDoc extends mongoose.Document {
+    message: string,
+    completed: boolean,
+}
+
+const  todoSchema = new mongoose.Schema({
+    message: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+todoSchema.statics.build = (attr: ITodo) => {
+    return new Todo(attr);
+};
+
+const Todo = mongoose.model<TodoDoc, TodoModelInterface>('Todo', todoSchema);
+
+export { Todo };
