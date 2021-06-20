5ba187ae80722f840142963d6f21f4ccb7dbf293
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-06-20 23:12:17 +0300

    feat: add POST, PATCH and DELETE methods

--- todolist-be/src/routes/todo.ts
+++ todolist-be/src/routes/todo.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-import { Todo } from '../models/todo';
+import express, {Request, Response} from 'express';
+import {Todo} from '../models/todo';
 
 const router = express.Router();
 
@@ -8,11 +8,47 @@ router.get('/api/todo', [], async (req: Request, res: Response) => {
     return res.status(200).send(todo);
 });
 
+router.get('/api/todo/:id', [], async (req: Request, res: Response) => {
+    const todoId = {_id: req.params.id};
+    try {
+        const todo = await Todo.findById(todoId);
+        return res.status(200).send(todo);
+    } catch (error) {
+        return res.status(404).send('');
+    }
+});
+
 router.post('/api/todo', async (req, res) => {
-    const { message, completed } = req.body;
+    const {message, completed} = req.body;
     const todo = Todo.build({message, completed});
     await todo.save();
     return res.status(201).send(todo);
 });
 
-export { router as todoRouter };
+router.patch('/api/todo', async (req, res) => {
+    const {_id, message, completed} = req.body;
+    const todoId = {_id: _id};
+    try {
+        let todo = await Todo.findByIdAndUpdate(todoId, {message: message, completed: completed});
+        return res.status(200).send(todo);
+    } catch (error) {
+        return res.status(400).send('');
+    }
+});
+
+router.delete('/api/todo', async (req, res) => {
+        const {_id} = req.body;
+        const todoId = {_id: _id};
+        try {
+            let todo = await Todo.findByIdAndDelete(todoId);
+            if(!todo) {
+                return res.status(404).send('');
+            }
+            return res.status(204).send('');
+        } catch (error) {
+            return res.status(404).send('');
+        }
+    }
+);
+
+export {router as todoRouter};
